95b95deb017ae721d9edb548cb8ba8591b9da009
zhourunlai <xiaorun95@gmail.com>
2019-06-20 20:38:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rsbeancount
+
+A tool to make the beancount file.
+
+## TODO
+
+- [ ] [Alipay](https://www.alipay.com/)
+- [ ] Wechat Pay
+- [ ] [China Merchants Bank](http://www.cmbchina.com/)(Salary card)
+
+## Reference
+
+1. [Beancount —— 命令行复式簿记](https://wzyboy.im/post/1063.html)
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+extern crate csv;
+
+use std::error::Error;
+use std::io;
+use std::process;
+
+fn example() -> Result<(), Box<Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
+}
